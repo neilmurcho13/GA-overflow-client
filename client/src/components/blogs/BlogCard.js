a40c5a4120129473edd1c4447803ef1c363de496
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-02 17:50:44 +0100

    sat 2nd oct end of day

--- client/src/components/blogs/BlogCard.js
+++ client/src/components/blogs/BlogCard.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+//NEED TO CHECK ENDPOINT FOR SINGLE POST
+//NEED TO ADD ENDPOINT TO COMMENTS
 
 const BlogCard = ({ _id, header, headerImgUrl, body }) => {
   return (
-    <div className='blog-card' id={_id}>
-      <div className='card-content'>
-        <img
-          src={headerImgUrl}
-          style={{ width: '200px', height: '200px' }}
-        />
-        <h4 className='blog-card-title'>{header}</h4>
-        <p className='blog-card-body'>{body}</p>
-        <button className='blog-card-btn'>Read more</button>
+    <div className="blog-card" id={_id}>
+      <div className="card-content">
+        <div className="info-container">
+          <h4 className="blog-card-title">{header}</h4>
+          <p className="blog-card-body">
+            {body.split(" ").slice(0, 10).join(" ")}...
+          </p>
+          <Link to={""} className="comments-btn">
+            <i class="fa fa-comments"></i>
+          </Link>
+          <br />
+          <button>
+            <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+          </button>
+          <Link to={"api/blogs/:id"} className="blog-card-btn">
+            read more
+          </Link>
+        </div>
+        <div className="image-container">
+          <img
+            className="card-image"
+            src={headerImgUrl}
+            style={{ width: "200px", height: "200px" }}
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogCard
+export default BlogCard;
