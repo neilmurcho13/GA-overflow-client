3656842bfb287926cb85febbcf144ddacd33e393
Dominic Fergal <dominicfergal@Dominics-MacBook-Air.local>
2021-10-09 11:51:38 +0100

    nav Link route changes

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,53 +1,57 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import gaLogo from "../../assets/ga-logo.png";
+import React, { useState } from 'react'
+import { useHistory, Link } from 'react-router-dom'
+import gaLogo from '../../assets/ga-logo.png'
 
 const Navbar = ({ onSearchChange }) => {
-  const history = useHistory();
-  const [searchInputVaule, setSearchInputValue] = useState("");
+  const history = useHistory()
+  const [searchInputVaule, setSearchInputValue] = useState('')
 
   const handleSearch = (e) => {
-    e.preventDefault();
-    onSearchChange(searchInputVaule);
-    history.push("/search");
-  };
+    e.preventDefault()
+    onSearchChange(searchInputVaule)
+    history.push('/search')
+  }
 
   return (
     <>
       <nav>
-        <div className="nav-content">
-          <div className="nav-logo">
-            <img src={gaLogo} />
-            <span>//</span>
-            <h2>OVERFLOW</h2>
-          </div>
+        <div className='nav-content'>
+          <Link to='/'>
+            <div className='nav-logo'>
+              <img src={gaLogo} />
+              <span>//</span>
+              <h2>OVERFLOW</h2>
+            </div>
+          </Link>
 
-          <div className="nav-links">
-            <form onSubmit={handleSearch} className="search-bar">
+          <div className='nav-links'>
+            <form onSubmit={handleSearch} className='search-bar'>
               <input
                 onChange={(e) => setSearchInputValue(e.target.value)}
                 value={searchInputVaule}
                 required
               />
-              <i className="fas fa-search"></i>
+              <i className='fas fa-search'></i>
             </form>
 
-            <div className="create-post">
-              <button>create post</button>
-              <i className="fas fa-pencil-alt"></i>
+            <div className='create-post'>
+              <Link to='/blogs/new'>
+                <button>create post</button>
+              </Link>
+              <i className='fas fa-pencil-alt'></i>
             </div>
-            <div className="my-profile">
+            <div className='my-profile'>
               <button>my profile</button>
-              <i className="far fa-user"></i>
+              <i className='far fa-user'></i>
             </div>
           </div>
         </div>
       </nav>
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
 
 // <div className="nav-links">
 // <div className="search-bar">
