9f16c53ec5892a61805edca2cbf5d0b2b31f5f63
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-12 13:29:12 +0100

    comments section incomplete

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,64 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link, useHistory } from 'react-router-dom'
-import { deleteBlog, getBlog } from '../../api/callerFunctions.js'
+import React, { useState, useEffect } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
+import { deleteBlog, getBlog } from "../../api/callerFunctions.js";
+import BlogComments from "./BlogComments.js";
 
 const BlogShow = () => {
-  const history = useHistory()
-  const { id } = useParams()
-  const [blogInfo, setBlogInfo] = useState([])
+  const history = useHistory();
+  const { id } = useParams();
+  const [blogInfo, setBlogInfo] = useState([]);
 
   // store the tags from this blogpost
-  const [tags, setTags] = useState([])
+  const [tags, setTags] = useState([]);
 
   useEffect(() => {
     getBlog(id).then((data) => {
-      setBlogInfo(data)
-      setTags(data.tags)
-    })
-  }, [])
+      setBlogInfo(data);
+      setTags(data.tags);
+    });
+  }, []);
 
   const handleDelete = async () => {
     try {
-      await deleteBlog(id)
-      history.push('/')
+      await deleteBlog(id);
+      history.push("/");
     } catch (err) {
-      console.error(`Failed to delete blog id: ${id}`, err)
+      console.error(`Failed to delete blog id: ${id}`, err);
     }
-  }
+  };
 
   return (
-    <div className='main-content'>
-      <img src={blogInfo.headerImgUrl} className='blog-header-img' />
-      <h1 className='blog-title'>{blogInfo.header}</h1>
-      <div className='social-links'>
-        <i className='fab fa-twitter-square'></i>
-        <i className='fab fa-instagram'></i>
-        <i className='fab fa-linkedin'></i>
-        <i className='fab fa-github-square'></i>
+    <div className="main-content">
+      <img src={blogInfo.headerImgUrl} className="blog-header-img" />
+      <h1 className="blog-title">{blogInfo.header}</h1>
+      <div className="social-links">
+        <i className="fab fa-twitter-square"></i>
+        <i className="fab fa-instagram"></i>
+        <i className="fab fa-linkedin"></i>
+        <i className="fab fa-github-square"></i>
       </div>
-      <h3 className='blog-summary'>{blogInfo.summary}</h3>
+      <h3 className="blog-summary">{blogInfo.summary}</h3>
       <hr />
-      <p className='blog-body'>{blogInfo.body}</p>
+      <p className="blog-body">{blogInfo.body}</p>
       <hr />
-      <div className='body-img'>
+      <div className="body-img">
         <img
           src={blogInfo.bodyImgUrl}
-          style={{ width: '100%', height: '30em', margin: '3em 0' }}
+          style={{ width: "100%", height: "30em", margin: "3em 0" }}
         />
       </div>
-      <div className='blog-tags'>
+      <div className="blog-tags">
         {tags.map((tag) => (
           <button key={tag}>{tag}</button>
         ))}
       </div>
-      <Link to={'/'}>
-        <button className='home-btn'>Home</button>
+
+      <BlogComments />
+
+      <Link to={"/"}>
+        <button className="home-btn">Home</button>
       </Link>
-      <button className='delete-blog-btn' onClick={handleDelete}>
+      <button className="delete-blog-btn" onClick={handleDelete}>
         Delete Blog
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default BlogShow
+export default BlogShow;
