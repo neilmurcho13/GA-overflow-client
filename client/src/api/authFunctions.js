eb96208fb4f58ad4601d14bd743fa11aab1491b8
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-12 21:41:25 +0100

    my profile fix

--- client/src/api/authFunctions.js
+++ client/src/api/authFunctions.js
@@ -1,49 +1,66 @@
+import axios from "axios";
+
 export const setToken = (token) => {
-  console.log('the token is ', token)
-  window.localStorage.setItem('token', token)
-  localStorage.setItem('user', JSON.stringify(users))
-}
+  console.log("the token is ", token);
+  window.localStorage.setItem("token", token);
+  localStorage.setItem("user", JSON.stringify(users));
+};
 
 export const getToken = () => {
-  return window.localStorage.getItem('token')
-}
+  return window.localStorage.getItem("token");
+};
 
 export const removeToken = () => {
-  return window.localStorage.getItem('token')
-}
+  return window.localStorage.getItem("token");
+};
 
 export const getPayLoad = () => {
-  const token = getToken()
+  const token = getToken();
   // this will return true or false
   if (!token) {
     // if the user isnt signed in
-    return false
+    return false;
   }
 
-  const parts = token.split('.')
+  const parts = token.split(".");
 
   if (parts.length < 3) {
     // if there is less than 3 '.' it mean you are not logged in
-    return false
+    return false;
   }
 
-  return JSON.parse(window.atob(parts[1]))
-}
+  return JSON.parse(window.atob(parts[1]));
+};
 
 export const isLoggedIn = () => {
-  const token = getToken()
+  const token = getToken();
 
   if (!token) {
     // if no token, you are not logged in
-    return false
+    return false;
   }
 
-  const parts = token.split('.')
+  const parts = token.split(".");
 
   if (parts.length < 3) {
     // if there is less than 3 '.' it mean you are not logged in
-    return false
+    return false;
   }
 
-  return JSON.parse(window.atob(parts[1]))
-}
+  return JSON.parse(window.atob(parts[1]));
+};
+
+export const getMyProfileData = async () => {
+  const options = {
+    method: "GET",
+    url: `http://localhost:3000/api/user`,
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTYxOWZkNTdlZGNlNjU4MDI2ZmM5ZmIiLCJpYXQiOjE2MzQwNjUzNDUsImV4cCI6MTYzNDEwODU0NX0.1kWZER8gMdflVkjGaQpIB_-C87FELaipr0kJvrWaUVg"
+    }
+  };
+
+  const { data } = await axios.request(options);
+
+  return data;
+};
--- client/src/components/common/MyProfile.js
+++ client/src/components/common/MyProfile.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { getMyProfileData } from "../../api/authFunctions";
 
 const MyProfile = () => {
+  const [profileData, setProfileData] = useState([]);
+
+  useEffect(() => {
+    getMyProfileData().then((data) => {
+      setProfileData(data);
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div>
       <h1>User profile page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default MyProfile
+export default MyProfile;
