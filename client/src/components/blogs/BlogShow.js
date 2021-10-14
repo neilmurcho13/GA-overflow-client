d9a943326bdd934050227ede03acf1f6143e8801
Dominic Fergal <dominicfergal@Dominics-MacBook-Air.local>
2021-10-14 21:28:09 +0100

    added createdAt to new blog

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,72 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, Link, useHistory } from 'react-router-dom';
-import { deleteBlog, getBlog } from '../../api/callerFunctions.js';
-import BlogComments from './BlogComments.js';
-import { getPayLoad } from '../../api/authFunctions.js';
-import { getUserById } from '../../api/callerFunctions.js';
+import React, { useState, useEffect } from 'react'
+import { useParams, Link, useHistory } from 'react-router-dom'
+import { deleteBlog, getBlog } from '../../api/callerFunctions.js'
+import BlogComments from './BlogComments.js'
+import { getPayLoad } from '../../api/authFunctions.js'
+import { getUserById } from '../../api/callerFunctions.js'
 
 const BlogShow = () => {
-  const history = useHistory();
-  const { id } = useParams();
-  const [blogInfo, setBlogInfo] = useState([]);
-  const [createdBy, setCreatedBy] = useState([]);
+  const history = useHistory()
+  const { id } = useParams()
+  const [blogInfo, setBlogInfo] = useState([])
+  const [createdBy, setCreatedBy] = useState([])
 
   useEffect(() => {
     getBlog(id).then((data) => {
-      setBlogInfo(data);
-    });
-  }, []);
+      setBlogInfo(data)
+    })
+  }, [])
 
   useEffect(() => {
     getBlog(id).then((data) => {
-      setCreatedBy(data.createdBy);
-    });
-  }, []);
+      setCreatedBy(data.createdBy)
+    })
+  }, [])
 
   useEffect(() => {
     getBlog(id).then((data) => {
-      setCreatedBy(data.createdBy);
-    });
-  }, []);
+      setCreatedBy(data.createdBy)
+    })
+  }, [])
 
-  // use later to conditionally render the delete button depending on user login
-  // if the id of this user is exactly the same of the craeted id of this cheese
-  const isOwner = getPayLoad().sub === blogInfo.ObjectId;
+  const isOwner = getPayLoad().sub === createdBy._id
 
   const handleDelete = async () => {
     try {
-      await deleteBlog(id);
-      history.push('/');
+      await deleteBlog(id)
+      history.push('/')
     } catch (err) {
-      console.error(`Failed to delete blog id: ${id}`, err);
+      console.error(`Failed to delete blog id: ${id}`, err)
     }
-  };
+  }
 
   return (
-    <div className="main-content">
-      <img src={blogInfo.headerImgUrl} className="blog-header-img" />
-      <h1 className="blog-title">{blogInfo.header}</h1>
-      <div className="social-links">
+    <div className='main-content'>
+      <img src={blogInfo.headerImgUrl} className='blog-header-img' />
+      <h1 className='blog-title'>{blogInfo.header}</h1>
+      <div className='social-links'>
         <a
           href={createdBy.linkedinLink}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          <i className="fab fa-linkedin"></i>
+          <i className='fab fa-linkedin'></i>
         </a>
         <a
           href={createdBy.githubLink}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          <i className="fab fa-github-square"></i>
+          <i className='fab fa-github-square'></i>
         </a>
       </div>
-      <h3 className="blog-summary">{blogInfo.summary}</h3>
+      <h3 className='blog-summary'>{blogInfo.summary}</h3>
       <hr />
-      <p className="blog-body">{blogInfo.body}</p>
+      <p className='blog-body'>{blogInfo.body}</p>
       <hr />
-      <div className="body-img">
+      <div className='body-img'>
         <img
           src={blogInfo.bodyImgUrl}
           style={{ width: '100%', height: '30em', margin: '3em 0' }}
@@ -77,24 +75,22 @@ const BlogShow = () => {
           <button key={tag}>{tag}</button>
         ))}
       </div> */}
-      {/* <h3 className='blog-summary'>
-        Created by : {blogInfo.createdBy.firstName}
-      </h3> */}
 
-      <div>Post by: {createdBy.username}</div>
+      <div>Posted by: {createdBy.username}</div>
+      <div>Posted on: {blogInfo.createdAt}</div>
 
       <BlogComments />
 
       <Link to={'/'}>
-        <button className="home-btn">Home</button>
+        <button className='home-btn'>Home</button>
       </Link>
       {isOwner && (
-        <button className="delete-blog-btn" onClick={handleDelete}>
+        <button className='delete-blog-btn' onClick={handleDelete}>
           Delete Blog
         </button>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default BlogShow;
+export default BlogShow
