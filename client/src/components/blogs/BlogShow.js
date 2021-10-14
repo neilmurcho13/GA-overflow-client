3721c2a39a83b9730f38cc9294127dc80f2a9c71
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-14 18:58:45 +0100

    createdby and reverse home

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,21 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link, useHistory } from 'react-router-dom'
-import { deleteBlog, getBlog } from '../../api/callerFunctions.js'
-import BlogComments from './BlogComments.js'
-import { getPayLoad } from '../../api/authFunctions.js'
-import { getUserById } from '../../api/callerFunctions.js'
+import React, { useState, useEffect } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
+import { deleteBlog, getBlog } from "../../api/callerFunctions.js";
+import BlogComments from "./BlogComments.js";
+import { getPayLoad } from "../../api/authFunctions.js";
+import { getUserById } from "../../api/callerFunctions.js";
 
 const BlogShow = () => {
-  const history = useHistory()
-  const { id } = useParams()
-  const [blogInfo, setBlogInfo] = useState([])
+  const history = useHistory();
+  const { id } = useParams();
+  const [blogInfo, setBlogInfo] = useState([]);
+  const [createdBy, setCreatedBy] = useState([]);
 
   useEffect(() => {
     getBlog(id).then((data) => {
-      setBlogInfo(data)
-      console.log(blogInfo)
-    })
-  }, [])
+      setBlogInfo(data);
+      console.log(blogInfo);
+    });
+  }, []);
+
+  useEffect(() => {
+    getBlog(id).then((data) => {
+      setCreatedBy(data.createdBy);
+      console.log("consolelog of created", data.createdBy);
+    });
+  }, []);
 
   // use later to conditionally render the delete button depending on user login
   // if the id of this user is exactly the same of the craeted id of this cheese
@@ -24,31 +32,31 @@ const BlogShow = () => {
 
   const handleDelete = async () => {
     try {
-      await deleteBlog(id)
-      history.push('/')
+      await deleteBlog(id);
+      history.push("/");
     } catch (err) {
-      console.error(`Failed to delete blog id: ${id}`, err)
+      console.error(`Failed to delete blog id: ${id}`, err);
     }
-  }
+  };
 
   return (
-    <div className='main-content'>
-      <img src={blogInfo.headerImgUrl} className='blog-header-img' />
-      <h1 className='blog-title'>{blogInfo.header}</h1>
-      <div className='social-links'>
-        <i className='fab fa-twitter-square'></i>
-        <i className='fab fa-instagram'></i>
-        <i className='fab fa-linkedin'></i>
-        <i className='fab fa-github-square'></i>
+    <div className="main-content">
+      <img src={blogInfo.headerImgUrl} className="blog-header-img" />
+      <h1 className="blog-title">{blogInfo.header}</h1>
+      <div className="social-links">
+        <i className="fab fa-twitter-square"></i>
+        <i className="fab fa-instagram"></i>
+        <i className="fab fa-linkedin"></i>
+        <i className="fab fa-github-square"></i>
       </div>
-      <h3 className='blog-summary'>{blogInfo.summary}</h3>
+      <h3 className="blog-summary">{blogInfo.summary}</h3>
       <hr />
-      <p className='blog-body'>{blogInfo.body}</p>
+      <p className="blog-body">{blogInfo.body}</p>
       <hr />
-      <div className='body-img'>
+      <div className="body-img">
         <img
           src={blogInfo.bodyImgUrl}
-          style={{ width: '100%', height: '30em', margin: '3em 0' }}
+          style={{ width: "100%", height: "30em", margin: "3em 0" }}
         />
       </div>
       {/* <div className="blog-tags">
@@ -60,16 +68,18 @@ const BlogShow = () => {
         Created by : {blogInfo.createdBy.firstName}
       </h3> */}
 
+      <div>Post by: {createdBy.username}</div>
+
       <BlogComments />
 
-      <Link to={'/'}>
-        <button className='home-btn'>Home</button>
+      <Link to={"/"}>
+        <button className="home-btn">Home</button>
       </Link>
-      <button className='delete-blog-btn' onClick={handleDelete}>
+      <button className="delete-blog-btn" onClick={handleDelete}>
         Delete Blog
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default BlogShow
+export default BlogShow;
