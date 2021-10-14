0207a75e73aff2c6f3cc42efa3a56ddc5f1a5226
Dominic Fergal <dominicfergal@Dominics-MacBook-Air.local>
2021-10-14 12:25:27 +0100

    style re-work

--- client/src/components/blogs/BlogCard.js
+++ client/src/components/blogs/BlogCard.js
@@ -4,30 +4,38 @@ import { Link } from 'react-router-dom'
 const BlogCard = ({ id, header, headerImgUrl, body }) => {
   return (
     <div className='blog-card' key={id}>
-      <div className='card-content'>
-        <div className='info-container'>
-          <h4 className='blog-card-title'>{header}</h4>
-          <p className='blog-card-body'>
-            {body.split(' ').slice(0, 10).join(' ')}...
-          </p>
-          <Link to={''} className='comments-btn'>
-            <i className='fa fa-comments'></i>
-          </Link>
-          <br />
-          <button>
-            <i className='fa fa-thumbs-up' aria-hidden='true'></i>
-          </button>
+      <div className='info-container'>
+        <h4 className='blog-card-title'>{header}</h4>
+        <div className='author-container'>
+          <span>by</span>
+          <h3>Dean Learner</h3>
+        </div>
+        <div className='date-container'>
+          <h3>13th Oct 2021</h3>
+        </div>
+        <p className='blog-body-container'>
+          {body.split(' ').slice(0, 20).join(' ')}...
+        </p>
+        <div className='links-container'>
           <Link to={`/blogs/${id}`} className='blog-card-btn'>
             read more
           </Link>
+          <div className='likes-container'>
+            <i className='fa fa-thumbs-up'></i>
+            <p>54</p>
+          </div>
+          <div className='comments-container'>
+            <i className='fa fa-comments'></i>
+            <p>12</p>
+          </div>
         </div>
-        <div className='image-container'>
-          <img
-            className='card-image'
-            src={headerImgUrl}
-            style={{ width: '200px', height: '200px' }}
-          />
-        </div>
+      </div>
+      <div className='image-container'>
+        <img
+          className='card-image'
+          src={headerImgUrl}
+          style={{ width: '225px', height: '160px' }}
+        />
       </div>
     </div>
   )
--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -15,35 +15,33 @@ const Header = ({ onSearchChange }) => {
   return (
     <>
       <header>
-        <div className='header-content'>
-          <Link to='/'>
-            <div className='header-logo'>
-              <img src={gaLogo} />
-              <span>//</span>
-              <h2>OVERFLOW</h2>
-            </div>
-          </Link>
-
-          <div className='header-links'>
-            <form onSubmit={handleSearch} className='search-bar'>
-              <input
-                type='search'
-                onChange={(e) => setSearchInputValue(e.target.value)}
-                value={searchInputVaule}
-                required
-              />
-              <i className='fas fa-search'></i>
-            </form>
-
-            <Link to='/blogs/new' className='create-post'>
-              <div>create post</div>
-              <i className='fas fa-pencil-alt'></i>
-            </Link>
-            <Link to='/user/profile' className='my-profile'>
-              <div>my profile</div>
-              <i className='far fa-user'></i>
-            </Link>
+        <Link to='/'>
+          <div className='header-logo'>
+            <img src={gaLogo} />
+            <span>//</span>
+            <h2>OVERFLOW</h2>
           </div>
+        </Link>
+
+        <div className='header-links'>
+          <form onSubmit={handleSearch} className='search-bar'>
+            <input
+              type='search'
+              onChange={(e) => setSearchInputValue(e.target.value)}
+              value={searchInputVaule}
+              required
+            />
+            <i className='fas fa-search'></i>
+          </form>
+
+          <Link to='/blogs/new' className='create-post'>
+            <div>create post</div>
+            <i className='fas fa-pencil-alt'></i>
+          </Link>
+          <Link to='/user/profile' className='my-profile'>
+            <div>my profile</div>
+            <i className='far fa-user'></i>
+          </Link>
         </div>
       </header>
     </>
