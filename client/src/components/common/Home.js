877db02e219d3a6170a6e01878c534e589daf0c6
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-15 21:52:42 +0100

    comments count now showing

--- client/src/components/common/Home.js
+++ client/src/components/common/Home.js
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import { getAllBlogs } from '../../api/callerFunctions.js'
-import BlogCard from '../blogs/BlogCard.js'
-import { motion } from 'framer-motion'
+import React, { useState, useEffect } from "react";
+import { getAllBlogs } from "../../api/callerFunctions.js";
+import BlogCard from "../blogs/BlogCard.js";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [blogs, setBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getAllBlogs().then((blogs) => {
-      setBlogs(blogs)
-      setIsLoading(false)
-    })
-  }, [])
+      setBlogs(blogs);
+      setIsLoading(false);
+    });
+  }, []);
 
   return (
     <motion.div
-      className='main-content'
+      className="main-content"
       animate={{ opacity: 1 }}
       initial={{ opacity: 0 }}
     >
-      <div className='header-container'>
+      <div className="header-container">
         <motion.h1
-          className='latest-header'
+          className="latest-header"
           animate={{ x: 0, opacity: 1 }}
           initial={{ x: 150, opacity: 0 }}
-          transition={{ type: 'tween', duration: 2 }}
+          transition={{ type: "tween", duration: 2 }}
         >
           Latest
         </motion.h1>
         <motion.div
-          className='latest-header-line'
+          className="latest-header-line"
           animate={{ x: 0, opacity: 1 }}
           initial={{ x: -900, opacity: 0 }}
-          transition={{ type: 'tween', duration: 2 }}
+          transition={{ type: "tween", duration: 2 }}
         ></motion.div>
       </div>
       {isLoading && <h2>Loading...</h2>}
       {blogs && (
-        <div className='blog-card-container'>
+        <div className="blog-card-container">
           {blogs
             .slice(0)
             .reverse()
@@ -49,12 +49,13 @@ const Home = () => {
                 header={blog.header}
                 headerImgUrl={blog.headerImgUrl}
                 body={blog.body}
+                comments={blog.comments}
               />
             ))}
         </div>
       )}
     </motion.div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
