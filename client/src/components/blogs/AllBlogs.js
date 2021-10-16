1dbb267b99bbae54c1fcf7a4fa535b4bd1070c49
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-16 10:48:57 +0100

    /all fix

--- client/src/components/blogs/AllBlogs.js
+++ client/src/components/blogs/AllBlogs.js
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import { getAllBlogs } from '../../api/callerFunctions'
-import BlogCard from './BlogCard'
-import { motion } from 'framer-motion'
+import React, { useState, useEffect } from "react";
+import { getAllBlogs } from "../../api/callerFunctions";
+import BlogCard from "./BlogCard";
+import { motion } from "framer-motion";
 
 const AllBlogs = () => {
-  const [blogs, setBlogs] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [blogs, setBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getAllBlogs().then((blogs) => {
-      setBlogs(blogs)
-      setIsLoading(false)
-    })
-  }, [])
+      setBlogs(blogs);
+      setIsLoading(false);
+    });
+  }, []);
 
   return (
     <motion.div
-      className='allblogs-content'
+      className="allblogs-content"
       animate={{ opacity: 1 }}
       initial={{ opacity: 0 }}
     >
-      <div className='header-container'>
+      <div className="header-container">
         <motion.h1
-          className='allblogs-header'
+          className="allblogs-header"
           animate={{ x: 0, opacity: 1 }}
           initial={{ x: -150, opacity: 0 }}
-          transition={{ type: 'tween', duration: 2 }}
+          transition={{ type: "tween", duration: 2 }}
         >
           All blogs
         </motion.h1>
         <motion.div
-          className='latest-header-line'
+          className="latest-header-line"
           animate={{ x: 0, opacity: 1 }}
           initial={{ x: -900, opacity: 0 }}
-          transition={{ type: 'tween', duration: 2 }}
+          transition={{ type: "tween", duration: 2 }}
         ></motion.div>
       </div>
       {isLoading && <h2>Loading...</h2>}
       {blogs && (
-        <div className='allblogs-card-container'>
+        <div className="allblogs-card-container">
           {blogs
             .slice(0)
             .reverse()
@@ -49,12 +49,15 @@ const AllBlogs = () => {
                 header={blog.header}
                 headerImgUrl={blog.headerImgUrl}
                 para1={blog.para1}
+                comments={blog.comments}
+                createdBy={blog.createdBy}
+                createdAt={blog.createdAt}
               />
             ))}
         </div>
       )}
     </motion.div>
-  )
-}
+  );
+};
 
-export default AllBlogs
+export default AllBlogs;
